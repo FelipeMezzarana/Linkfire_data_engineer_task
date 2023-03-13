913271a45e8ace1791b78f6b46f741e687bfb734
Felipe S. Mezzarana <90487618+FelipeMezzarana@users.noreply.github.com>
2023-03-13 20:32:58 -0300

    Add function to generate report with the answers to all the questions in step 5

--- output_report.py
+++ output_report.py
@@ -116,6 +116,76 @@ def create_invalid_data_report():
         counter+=1
                 
 
-if __name__ == '__main__':
+def create_analytical_report():
+    """Creates a txt file "analytical_report" with the answers to all the questions in step 5
+    """     
+
+    today = datetime.today().strftime("%Y-%m-%d %H-%M")
+    file_name = 'reports/analytical_report_' + today + '.txt'
+    
+    # 1º Query
+    query_1_path = 'analysis_queries/01_most_common_first_name.sql'
+    most_common_first_name_df = query_to_df(query_1_path)
+    # Write results
+    with open(file_name, 'w', encoding='utf-8') as f:
+        f.write(f'01 - What is the most common first name among actors and actresses?\n')
+        f.write(f'{most_common_first_name_df.first_name[0]} - {most_common_first_name_df.first_name_qty[0]} Records')
+        
+    # 2º Query   
+    query_2_path = 'analysis_queries/02_movie_with_longest_timespan.sql'
+    movie_with_longest_timespan = query_to_df(query_2_path)
+    # Checking duplicate values in first position (draw)
+    draw_qty = movie_with_longest_timespan.loc[
+        movie_with_longest_timespan.year_timespan == max(movie_with_longest_timespan.year_timespan),[
+        'date_added', 'release_year', 'year_added', # Loc cols related to date
+        'month_added', 'day_added', 'year_timespan']].duplicated().sum() + 1 # No draw = 0 duplicates
+    # Write results
+    with open(file_name, 'a', encoding='utf-8') as f:
+        f.write(f'\n\n02 - Which Movie had the longest timespan from release to appearing on Netflix?\n')
+        for i in range(draw_qty): # loop through draw values
+            f.write(f'{movie_with_longest_timespan.title[i]} - '
+                    f'release_year {movie_with_longest_timespan.release_year[i]} '
+                    f'date_added {movie_with_longest_timespan.date_added[i]}\n')
+    
+    # 3º Query
+    query_3_path = 'analysis_queries/03_month_most_new_releases.sql'
+    month_most_new_releases = query_to_df(query_3_path)
+    # Write results
+    with open(file_name, 'a', encoding='utf-8') as f:
+        f.write(f'\n03 - Which Month of the year had the most new releases historically?\n')
+        f.write(f'Month {month_most_new_releases.month_added[0]:.0f} - '
+                f'{month_most_new_releases.qty_titles_added[0]} Titles added')
+    
+    # 4º Query
+    query_4_path = 'analysis_queries/04_tv_shows_largest_increase_year_on_year.sql'
+    tv_shows_largest_increase = query_to_df(query_4_path)
+    # Write results
+    with open(file_name, 'a', encoding='utf-8') as f:
+        f.write(f'\n\n04 - Which year had the largest increase year on year (percentage wise) for TV Shows?\n')
+        f.write(f'{tv_shows_largest_increase.year_added[0]:.0f} - '
+                f'{tv_shows_largest_increase.increase_percent[0]:.2f}% Increase percent.')
+
+    # 5º Query
+    query_5_path = 'analysis_queries/05_actresses_more_than_one_movie_with_wood.sql'
+    actresses_with_woody = query_to_df(query_5_path)
+    # Write results
+    with open(file_name, 'a', encoding='utf-8') as f:
+        f.write(f'\n\n05 - List the actresses that have appeared in a movie with Woody Harrelson more than once?\n')
+        for i in range(len(actresses_with_woody)):
+            f.write(f'{actresses_with_woody.cast_member[i]} - '
+                    f'{actresses_with_woody.qty_movies_with_woody[i]:.0f} Movies with Woody Harrelson\n')
+
+
+def reports_pipeline():
+    """Generates three reports on folder Linkfire_data_engineer_task/reports
+    """ 
+    
     create_null_values_report()
     create_invalid_data_report()
+    create_analytical_report()
+
+
+if __name__ == '__main__':
+    #create_null_values_report()
+    #create_invalid_data_report()
+    create_analytical_report()
